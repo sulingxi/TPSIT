357532d67a8279dd9fe1880843c279e13b2d7ce9
su leo <157701661+sulingxi@users.noreply.github.com>
2026-03-19 01:19:26 +0100

    Refactor Todo class to include id and noteId

--- zkeep/model.dart
+++ zkeep/model.dart
@@ -1,5 +1,52 @@
+// classe per una nota
+class Note {
+  Note({required this.id, required this.title});
+
+  final int? id;
+  final String title;
+
+  // trasformo oggetto in mappa per database
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'title': title};
+  }
+
+  // prendo una mappa e faccio oggetto note
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(id: map['id'], title: map['title']);
+  }
+}
+
+// classe per un promemoria dentro una nota
 class Todo {
-  Todo({required this.name, required this.checked});
+  Todo({
+    required this.id,
+    required this.noteId,
+    required this.name,
+    this.checked = false,
+  });
+
+  final int? id;
+  final int noteId;
   final String name;
   bool checked;
+
+  // trasformo oggetto in mappa per database
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'note_id': noteId,
+      'name': name,
+      'checked': checked ? 1 : 0,
+    };
+  }
+
+  // prendo una mappa e faccio oggetto todo
+  factory Todo.fromMap(Map<String, dynamic> map) {
+    return Todo(
+      id: map['id'],
+      noteId: map['note_id'],
+      name: map['name'],
+      checked: map['checked'] == 1,
+    );
+  }
 }
